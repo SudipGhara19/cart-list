29f6952c4aff1291a775c05a9044c44a9504d79f
SudipGhara19 <sudipghara135@gmail.com>
2023-04-10 01:07:05 +0530

    added icons and constractor for cart list

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,18 @@
 import React from "react";
 class CartItem extends React.Component{
+    
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            Qty: '1',
+            img: ''
+        }
+    }
+
     render(){
+        const {price, title, Qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -8,11 +20,14 @@ class CartItem extends React.Component{
                 </div>
 
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs: 10000</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25}}> {title} </div>
+                    <div style={{color: '#777'}}>Rs: {price} </div>
+                    <div style={{color: '#777'}}>Qty: {Qty} </div>
                     <div className="cart-item-actions"></div>
                      {/* buttons ^ */}
+                     <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/561/561169.png"/>
+                     <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/561/561179.png"/>
+                     <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/484/484611.png"/>
                 </div>
             </div>
         );
