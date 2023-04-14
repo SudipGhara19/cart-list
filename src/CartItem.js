82ff245090fdc5306db4ac2349d34eaee0f63c2c
SudipGhara19 <sudipghara135@gmail.com>
2023-04-14 18:31:42 +0530

    added delete method

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,6 +4,13 @@ class CartItem extends React.Component {
   render () {
     console.log('this.props', this.props);
     const { price, title, qty } = this.props.product;
+    const {
+        product, 
+        onIncreaseQuantity, 
+        onDecreaseQuantity, 
+        onDeleteProduct
+    } = this.props
+
     return (
       <div className="cart-item">
         {this.props.jsx}
@@ -20,18 +27,19 @@ class CartItem extends React.Component {
               alt="increase"
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/561/561169.png"
-              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/561/561179.png"
-              onClick={() => this.props.onDecreaseQuantity(this.props.product)}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
-              src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
+              src="https://cdn-icons-png.flaticon.com/512/484/484611.png" 
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
