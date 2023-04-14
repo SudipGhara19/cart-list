ac5520d7d6eb02fe65d632d9bddb0687bf66f009
SudipGhara19 <sudipghara135@gmail.com>
2023-04-14 23:50:43 +0530

    moving state cart to app

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = (props) => {
     <div style={styles.nav}>
       <div style={styles.cartIconContainer}>
         <img style={styles.cartIcon} src="https://uxwing.com/wp-content/themes/uxwing/download/e-commerce-currency-shopping/shopping-cart-icon.png" alt="cart-icon" />
-        <span style={styles.cartCount}>3</span>
+        <span style={styles.cartCount}> {props.count} </span>
       </div>
     </div>
   );
